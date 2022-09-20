7084086aced848c3fd55cb1bd5a1bf8044ede792
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2022-09-20 23:22:01 +0530

    Token Price Fetch

--- bot.py
+++ bot.py
@@ -38,10 +38,32 @@ def motivate(update, context):
     quote = requests.request(url='https://api.quotable.io/random',method='get')
     update.message.reply_text(quote.json()['author'])
 
+def tokenprice(update, context):
+    quote = requests.request(url='https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/pools/?quote-currency=USD&format=JSON&contract-addresses=0x4ae2cd1f5b8806a973953b76f9ce6d5fab9cdcfd&key=ckey_7e494e0bde414fd19967dfc3586',method='get')
+    update.message.reply_text(quote.json()["data"]["items"][0]["token_0"]["quote_rate"])
+
+def gettokenprice(update, context: CallbackContext):
+	geturl="https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/pools/?quote-currency=USD&format=JSON&contract-addresses="+context.args[0]+"&key=ckey_7e494e0bde414fd19967dfc3586"
+	print(geturl)
+	quote = requests.request(url=geturl,method='get')
+	if(quote.json()["data"]["items"][0]["token_0"]["quote_rate"]):
+		update.message.reply_text(quote.json()["data"]["items"][0]["token_0"]["quote_rate"])
+	else:
+		update.message.reply_text("Can not get price")	
+
+def liquidity(update, context):
+    quote = requests.request(url='https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/pools/?quote-currency=USD&format=JSON&contract-addresses=0x4ae2cd1f5b8806a973953b76f9ce6d5fab9cdcfd&key=ckey_7e494e0bde414fd19967dfc3586',method='get')
+    update.message.reply_text(quote.json()["data"]["items"][0]["total_liquidity_quote"])
+	
+
 def wbnb(update, context):
     quote = requests.request(url='https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c&apikey=Q8YP1QDJBZ537SB47ECQGW7UPIAFE2VTZZ',method='get')
     update.message.reply_text(quote.json()['status'])        
 
+
+def checkcontext(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		" you said '%s'" % context.args)
 
 def unknown_text(update: Update, context: CallbackContext):
 	update.message.reply_text(
@@ -56,6 +78,10 @@ def callback_30(context: CallbackContext):
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('motivate', motivate))
+updater.dispatcher.add_handler(CommandHandler('price', tokenprice))
+updater.dispatcher.add_handler(CommandHandler('getprice', gettokenprice))
+updater.dispatcher.add_handler(CommandHandler('repeat', checkcontext))
+updater.dispatcher.add_handler(CommandHandler('liquidity', liquidity))
 updater.dispatcher.add_handler(CommandHandler('wbnb', wbnb))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown)) # Filters out unknown commands
